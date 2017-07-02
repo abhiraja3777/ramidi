021a2253d410c833549b4327c308b41d7cb22ac8
ca <abhiraja3777@gmail.com>
2017-07-02 21:18:07 +0200

    hell

---
+++ sell.py
@@ -0,0 +1,9 @@
+from pyvirtualdisplay import Display
+from selenium import webdriver
+
+display = Display(visible=0, size=(800,600))
+display.start
+
+browser = webdriver.Firefox()
+
+browser.get("h")
